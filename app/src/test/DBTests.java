98f8f8dca8bf9d744292be38377c61262805e0cd
vha4493 <vha4493@gmail.com>
2026-04-26 21:35:14 -0700

    DB Tests, gradle dependency, and sqlite sprouts

---
+++ app/src/test/DBTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DBTests {
+    private userDao;
+    private UserDB db;
+
+    @BeforeEach
+    void setUp() {
+        public void createDb(){
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void userInsert() {
+        String username = "SenorSour;
+        String password = "unomas";
+        User user = new User(username, password);
+        userDao.insert(user);
+        List<User> users = userDao.getAllUsersList();
+        assertNotNull(users.get(0));
+        assertEquals(username, users.get(0).getUsername());
+        // write your test here
+    }
+}
